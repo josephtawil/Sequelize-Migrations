a5be8a022200e1b9e6271a3452b5b62f7dee893a
Joseph Tawil <jtawil2015@gmail.com>
2020-07-02 05:19:03 -0700

    removed useless migration file

--- migrations/20200702111253-first_name.js
+++ migrations/20200702111253-first_name.js
@@ -10,11 +10,11 @@ module.exports = {
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
-    await queryInterface.renameColumn('students', "fname", "first_name");
+    // await queryInterface.renameColumn('students', "fname", "first_name");
+    await queryInterface.renameColumn('students', "first_name", "first_name");
+    // await queryInterface.addColumn("students", "last_name", { type: Sequelize.STRING, allowedNull: false });
 
-    await queryInterface.addColumn("students", "last_name", { type: Sequelize.STRING });
-
-    await queryInterface.sequelize.query("UPDATE students SET last_name = Tawil");
+    // await queryInterface.sequelize.query(`UPDATE students SET last_name = "Tawil";`);
   },
 
   down: async (queryInterface, Sequelize) => {
@@ -24,6 +24,7 @@ module.exports = {
      * Example:
      * await queryInterface.dropTable('users');
      */
+    await queryInterface.renameColumn('students', "first_name", "fname");
     await queryInterface.removeColumn("students", "last_name");
   }
 };
